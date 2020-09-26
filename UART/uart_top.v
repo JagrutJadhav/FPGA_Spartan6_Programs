c7d6adfd7c0695151caf7d5025871d1fd88e6398
Jagrut Jadhav <64086840+JagrutJadhav@users.noreply.github.com>
2020-09-26 10:58:31 +0530

    Update uart_top.v

--- UART/uart_top.v
+++ UART/uart_top.v
@@ -12,61 +12,56 @@
 // 9600 <= 1250clk <= 0x4E2
 // 12500<= 104clk  <= 0x068
 //////////////////////////////////////////////////////////////////////////////////
-module uart_top( input clk,
-					input rst,
-					 output tx 
-					);
- 
+module uart_top( 	input 				clk,					//system-clk signal
+									input 				rst,					//reset signal active-high
+									input 				rx_data,			//rx signal input from UART
+									input 				tx_enable,		//enable pin to transmit data to activate UART tx. ENABLE SHOULD BE KEPT HIGH UNTIL data is transmitted
+									input 	[7:0] tx_parallel,	//8_bit parallel data to bve transmitted to tx
+									output 	[7:0] rx_parallel,	//8 bit parallel data fetched from rx 
+									output 				rx_ready,			//indication that we can get the data from rx.(ready = 0 means that it is receieving data from UART and is between process)
+									output 				tx_ready,			//indication that tx is ready to input value.(ready = 0 means it is transmitting previous data and is between process)
+									output 				tx_data				//tx signal out to UART
+								);
+ //Baud generator sognals
  wire baud_rate;
  wire baud_clk;
  wire enable;
+ //tx signals
  reg [7:0] din;
- wire tx_out_en;
  wire ready;
- wire data ;
- reg tx_enable = 1'b0;
- reg rx_enable =1'b0;
- reg [2:0] state =3'b000;
+ //rx signals
+ wire rx_ready;
+ wire rx_in_en;
+ wire [7:0] dout;
+ 
  baud_gen baud						// module to generate baudrate
-		   (.clk(clk),
-			.ena(enable),
-			.rst(rst),
-			.baud_clk);
+				(.clk(clk),
+				.ena(enable),
+				.rst(rst),
+				.baud_clk
+				);
  
 
- tx trans (.din(din),			// module tx instantiated
+ tx trans (.din(tx_parallel),			// module tx instantiated
 			 .tx_out_en(tx_enable),
-			 .baud_clk,
+			 .baud_clk(baud_clk),
 			 .rst(rst),
 			 .clk(clk),
-			 .ready(ready),
+			 .ready(tx_ready),
 			 .data(data)
-    );	 
-always @ (posedge clk, posedge rst)
-begin
-	if(rst)
-	begin
-		tx_enable <= 1'b0;
-	end
-	else 
-	begin
-		if (ready == 1'b1)
-		begin
-			case (state)
-			3'b000:begin
-						din <= 8'b01000001;
-						state <= 3'b001;
-					 end
-			3'b001: begin
-						tx_enable <= 1'b1;
-						end
-			default : state <= 3'b000;
-			endcase
-		end
-	end
+			);
 
-end
-assign enable = tx_enable | rx_enable;
-assign tx = tx_enable ? data:1'b1;
+ rx reci (                 //module rx 
+		.data(rx_data), 
+		.baud_clk(baud_clk), 
+		.rst(rst), 
+		.clk(clk), 
+		.rx_ready(rx_ready), 
+		.rx_in_en(rx_in_en), 
+		.dout(dout)
+		);	 
+
+assign rx_parallel = dout;
+assign enable = tx_enable | rx_in_en;
+assign tx_data = tx_enable ? data:1'b1;
 endmodule
-
