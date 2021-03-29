1f40a287873fb2d9aea3387a13a320a50d82e927
Jagrut Jadhav <64086840+JagrutJadhav@users.noreply.github.com>
2021-03-28 21:28:02 -0400

    Update tx.v

--- UART/tx.v
+++ UART/tx.v
@@ -4,12 +4,12 @@
 // Module Name:    tx 
 //////////////////////////////////////////////////////////////////////////////////
 module tx(input [7:0] din,
-			 input tx_out_en,
-			 input baud_clk,
-			 input rst,
-			 input clk,
-			 output ready,
-			 output data
+	 input tx_out_en,
+	 input baud_clk,
+	 input rst,
+	 input clk,
+	 output ready,
+	 output data
     );
 parameter st_bit = 4'b0000;
 parameter bit_1  = 4'b0001;
@@ -39,56 +39,56 @@ begin
 	else if(enable)
 	begin
 		
-		case (state)
-			idle   : begin
-						tx_ready <= 1'b1;
-						dout <= 1'b1;
-						state <= st_bit;
-						end
-			st_bit : begin
-						tx_ready <= 1'b0;
-						dout <= 1'b0;
-						state <= bit_1;
-						end
-			bit_1  : begin
-						dout <= tx_buff[0];
-						state <= bit_2;
-						end
-			bit_2  : begin
-						dout <= tx_buff[1];
-						state <= bit_3;
-						end
-			bit_3  : begin
-						dout <= tx_buff[2];
-						state <= bit_4;
-						end
-			bit_4  : begin
-						dout <= tx_buff[3];
-						state <= bit_5;
-						end
-			bit_5  : begin
-						dout <= tx_buff[4];
-						state <= bit_6;
-						end
-			bit_6  : begin
-						dout <= tx_buff[5];
-						state <= bit_7;
-						end
-			bit_7  : begin
-						dout <= tx_buff[6];
-						state <= bit_8;
-						end
-			bit_8  : begin
-						dout <= tx_buff[7];
-						state <= stop;
-						end
-			stop   : begin
-						dout <= 1'b1;
-						state <= idle;
-						tx_ready <= 1'b1;
-						end
-			default : dout <= 1'b1;
-		endcase	
+	case (state)
+		idle   : begin
+			 tx_ready <= 1'b1;
+			 dout <= 1'b1;
+			 state <= st_bit;
+			 end
+		st_bit : begin
+			 tx_ready <= 1'b0;
+			 dout <= 1'b0;
+			 state <= bit_1;
+			 end
+		bit_1  : begin
+			 dout <= tx_buff[0];
+			 state <= bit_2;
+			 end
+		bit_2  : begin
+			 dout <= tx_buff[1];
+			 state <= bit_3;
+			 end
+		bit_3  : begin
+			 dout <= tx_buff[2];
+			 state <= bit_4;
+			 end
+	        bit_4  : begin
+			 dout <= tx_buff[3];
+			 state <= bit_5;
+			 end
+	        bit_5  : begin
+			 dout <= tx_buff[4];
+			]state <= bit_6;
+			 end
+		bit_6  : begin
+			 dout <= tx_buff[5];
+			 state <= bit_7;
+			 end
+		bit_7  : begin
+			 dout <= tx_buff[6];
+			 state <= bit_8;
+			 end
+		bit_8  : begin
+			 dout <= tx_buff[7];
+			 state <= stop;
+			 end
+		stop   : begin
+			 dout <= 1'b1;
+			 state <= idle;
+			 tx_ready <= 1'b1;
+			 end
+	        default : dout <= 1'b1;
+	endcase	
 	end
 end	
 always @ (posedge clk, posedge rst)	
